f4b6dee0790886b7a3a454fb24b1141457dfd0a5
ihgann <ian.gann@berkeley.edu>
2015-04-27 18:44:08 -0700

    Final push to make the markdown and code syntax highlighting.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,24 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+class CodeRayify < Redcarpet::Render::HTML
+  def block_code(code, language)
+    CodeRay.scan(code, language).div
+  end
+end
+
+def markdown(text)
+  language ||= :plaintext
+  coderayified = CodeRayify.new(:filter_html => true, :hard_wrap => true)
+  options = {
+    :fenced_code_blocks => true,
+    :no_intra_emphasis => true,
+    :autolink => true,
+    :lax_html_blocks => true,
+  }
+  markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+  markdown_to_html.render(text).html_safe
+end
+
 end
