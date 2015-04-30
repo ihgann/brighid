15fe3fdaaee2bb628605c462fd2d25567ff21ff3
ihgann <ian.gann@berkeley.edu>
2015-04-30 13:51:37 -0700

    change route for facebook callback

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "home#index"
   # devise_for :users
   devise_for :users, controllers: { registrations: 'registrations', :omniauth_callbacks => "users/omniauth_callbacks" }
+  get '/users/auth/facebook/callback', to: redirect('/accounts/auth/facebook/callback')
   resources :courses
   resources :notes
   get "about" => "home#about"
