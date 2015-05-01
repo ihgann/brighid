121c903cb77ace19110b3ac3db5f11c18049a65f
ihgann <ian.gann@berkeley.edu>
2015-05-01 00:39:36 -0700

    Changed production routes

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,6 +1,13 @@
 WickedPdf.config = {
   #:wkhtmltopdf => '/Users/ihgann/.rvm/gems/ruby-2.1.2@rails_decal/bin/wkhtmltopdf'
-  :wkhtmltopdf => '/usr/local/bin/wkhtmltopdf' 
+  #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf' 
   #:layout => "pdf.html",
   #:exe_path => '/usr/local/bin/wkhtmltopdf'
 }
+
+if Rails.env.staging? || Rails.env.production?
+  WickedPdf.config = {:exe_path => Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s }
+else
+  WickedPdf.config = {:exe_path => '/usr/local/bin/wkhtmltopdf' }
+end
+
